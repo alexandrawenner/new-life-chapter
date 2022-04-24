077a6fc259fbab21aa7c68654d79ef63368dd8e5
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-04-24 16:03:27 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Thanks for visiting!";
 document.querySelector("body").appendChild(h2);
+
+
+
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
